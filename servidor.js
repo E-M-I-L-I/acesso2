9b952487c804166bd2400ac95d5a51bc08ab7ad0
E-M-I-L-I <emili.z@aluno.ifsc.edu.br>
2022-07-25 11:41:33 +0000

    Mudar URL do jogo para Heroku

--- servidor.js
+++ servidor.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: {
-    origins: ["https://cliente.ifsc.cloud"],
+    origins: ["https://vento.ifsc.cloud"],
   },
 });
 const PORT = process.env.PORT || 3000;
